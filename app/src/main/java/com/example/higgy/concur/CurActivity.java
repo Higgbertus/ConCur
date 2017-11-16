39f52813fbf4366e3fedd50ecbe06d0d871ce79c
Fabi <naass@mail.hs-ulm.de>
2017-11-16 15:14:05 +0100

    intent shit

--- app/src/main/java/com/example/higgy/concur/CurActivity.java
+++ app/src/main/java/com/example/higgy/concur/CurActivity.java
@@ -1,7 +1,11 @@
 package com.example.higgy.concur;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class CurActivity extends AppCompatActivity {
@@ -11,7 +15,7 @@ public class CurActivity extends AppCompatActivity {
     private ExchangeRateDatabase exchangeRateDB;
     private ListView listView;
     private CurrencyItemAdapter currencyItemAdapter;
-
+private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +26,18 @@ public class CurActivity extends AppCompatActivity {
         currencyItemAdapter = new CurrencyItemAdapter(exchangeRateDB);
         conAdapter = new ConAdapter(exchangeRateDB);
         listView.setAdapter(currencyItemAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0`?q="+exchangeRateDB.getCapital(exchangeRateDB.getCurrencies()[i])));
+
+                if (intent.resolveActivity(getPackageManager())!= null){
+                    startActivity(intent);
+                }
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/higgy/concur/MainActivity.java
+++ app/src/main/java/com/example/higgy/concur/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.higgy.concur;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,7 +16,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private ExchangeRateDatabase exchangeRateDB;
-    private ConAdapter conAdapter;
     private CurrencyItemAdapter currencyItemAdapter;
     private Spinner spinnerFrom,spinnerTo;
     private EditText editText;
@@ -22,13 +23,12 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
     private Intent intent;
-
+    ShareActionProvider shareActionProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         exchangeRateDB = new ExchangeRateDatabase();
-        conAdapter = new ConAdapter(exchangeRateDB);
         currencyItemAdapter = new CurrencyItemAdapter(exchangeRateDB);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -46,21 +46,38 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         intent = new Intent(MainActivity.this, CurActivity.class);
-        
-        startActivity(intent);
+        if (intent.resolveActivity(getPackageManager())!= null){
+            startActivity(intent);
+        }
         return true;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        setShareText(null);
         return true;
     }
 
+    private void setShareText(String text) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        if (text != null){
+            shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+        }
+
+        shareActionProvider.setShareIntent(shareIntent);
+    }
+
+
+
     public void onClick(View view){
         textView.setText(String.format("%.2f", exchangeRateDB.convert(Double.parseDouble(editText.getText().toString()),spinnerFrom.getSelectedItem().toString(),spinnerTo.getSelectedItem().toString())));
+        setShareText((String) textView.getText());
     }
 
 }
